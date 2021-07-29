629185d15faae70101724d4458fee6f27e4ae493
sarah-pardo <spardo2@wellesley.edu>
2021-07-29 18:26:09 -0400

    added server = app.server

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 #"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css", integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T", crossorigin="anonymous"]
 #'https://codepen.io/chriddyp/pen/bWLwgP.css',"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
-
+server=app.server
             # Container for all layout
 app.layout = dbc.Container(
         #children in container
